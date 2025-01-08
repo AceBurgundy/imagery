5040cc6129bc28f5bf5dec12a20bc2d53f366e61
AceBurgundy <samadriansabalo99@gmail.com>
2025-01-08 15:00:28 +0800

    added send ipc.

--- src/preload.js
+++ src/preload.js
@@ -2,5 +2,6 @@ const {contextBridge, ipcRenderer} = require('electron');
 
 contextBridge.exposeInMainWorld('ipcRenderer', {
   invoke: (channel, data) => ipcRenderer.invoke(channel, data),
+  send: (channel, data) => ipcRenderer.send(channel, data),
   on: (channel, callback) => ipcRenderer.on(channel, callback)
 });
