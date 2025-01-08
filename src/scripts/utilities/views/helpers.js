9367b73679a628c9a2ce4d5ea553730781e1bf8a
AceBurgundy <samadriansabalo99@gmail.com>
2025-01-08 15:04:55 +0800

    moved some handlers here.

--- src/scripts/utilities/views/helpers.js
+++ src/scripts/utilities/views/helpers.js
@@ -1,6 +1,7 @@
 const { createLogger, transports: _transports } = require('winston');
-const { nativeImage, app } = require('electron');
-const { extname, basename } = require('path');
+const { nativeImage, app, ipcMain, BrowserWindow } = require('electron');
+const { extname, basename, join } = require('path');
+const path = require('path');
 
 // Enum for object types
 const FileGroup = {
@@ -26,7 +27,7 @@ function isImageFile(fileName) {
  * @returns {boolean}
  */
 function isVideoFile(fileName) {
-  const videoExtensions = ['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.webm'];
+  const videoExtensions = ['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.webm', '.ts'];
   return videoExtensions.includes(extname(fileName).toLowerCase());
 }
 
@@ -39,16 +40,18 @@ function isMediaFile(fileName) {
   return isImageFile(fileName) || isVideoFile(fileName);
 }
 
+const defaultImagePath = path.resolve(app.getAppPath(), "src/assets/images/load.webp").replace(/\\/g, '/');
+
 /**
  * Creates a thumbnail from a given media path.
  * @param {string} filePath - Path of the file to check.
  * @param {number} size - Size of the thumbnail.
  * @returns {Promise<String>} The base64 image thumbnail
  */
-async function getThumbnail(filePath, size) {
+ipcMain.handle('get-thumbnail', async (_, filePath, size) => {
   if (!filePath) {
     logError("Cannot pass an undefined file path to getThumbnail");
-    return;
+    return defaultImagePath;
   }
 
   try {
@@ -60,11 +63,9 @@ async function getThumbnail(filePath, size) {
     return media.toDataURL();
   } catch (error) {
     logError(`Error in getting file thumbnail for ${basename(filePath)}, getting default thumbnail instead: ${error}`);
-
-    const thumbnail = await app.getFileIcon(filePath);
-    return thumbnail.toDataURL();
+    return defaultImagePath;
   }
-}
+});
 
 const logger = createLogger({
   transports: [
@@ -77,9 +78,29 @@ const logger = createLogger({
  * @param {Error | string} error - an instance of Error or a string
  */
 function logError(error) {
-  console.log(error);
+  // console.log(error);
   logger.error(`${new Date().toISOString()} : ${error}`);
 }
+
+ipcMain.handle('print', (_, message) => {
+  console.log(message.join(' '));
+});
+
+ipcMain.handle('join-paths', (_, paths) => {
+  return join(...paths);
+});
+
+ipcMain.handle('path-basename', (_, _path) => {
+  return basename(_path);
+});
+
+ipcMain.handle('on-fullscreen', () => {
+  BrowserWindow.getFocusedWindow().setFullScreen(true);
+})
+
+ipcMain.handle('off-fullscreen', () => {
+  BrowserWindow.getFocusedWindow().setFullScreen(false);
+})
 
 module.exports = {
   FileGroup,
@@ -87,5 +108,4 @@ module.exports = {
   logError,
   isVideoFile,
   isMediaFile,
-  getThumbnail
 };
