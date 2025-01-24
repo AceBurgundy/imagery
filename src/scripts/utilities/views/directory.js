6633214555174aa050ee599580a42b2494355dbb
AceBurgundy <Samadriansabalo99@gmail.com>
2025-01-24 21:56:46 +0800

    Major removal and refactorization of some code.

--- src/scripts/utilities/views/directory.js
+++ src/scripts/utilities/views/directory.js
@@ -1,182 +1,38 @@
-const { FileGroup, isMediaFile, isVideoFile, logError } = require("./helpers.js");
+const { isMediaFile } = require("./helpers.js");
 const { existsSync, promises } = require("fs");
-const { join } = require("path");
-const path = require('path');
-const { app, ipcMain } = require('electron');
-
-defaultThumbnailPath = path.resolve(app.getAppPath(), "src/assets/images/load.webp").replace(/\\/g, '/');
-let activeStreamId = null; // Keeps track of the active stream
-
-let pathBeingProcessed = '';
-let pathEntries = [];
-let slicedEntries = [];
-let processedEntries = [];
-let index = -1;
-
-ipcMain.handle('default-image', _ => defaultThumbnailPath);
+const { ipcMain } = require('electron');
+const { imageryCache } = require('./imagery-cache.js');
 
 /**
- * Generates an array of objects for a given directory path.
- * @param {string} folderPath - The path to the directory to process.
+ * Generates an array of objects for a given directory path and creates a cache of the directory contents.
+ * @param {string} path - The path to the directory to process.
+ * @throws {Error} - Throws an error if the folder path does not exist.
  */
-ipcMain.handle('get-directory-contents', async (event, data) => {
-  const folderPath = data[0];
-  let startIndex = Number(data[1]);
-  let endIndex = Number(data[2]);
-
-  const streamId = Date.now();
-  activeStreamId = streamId;
-
-  processedEntries = [];
-
-  if (!existsSync(folderPath) === true) {
+ipcMain.handle('prepare-directory-contents', async (event, path) => {
+  if (!existsSync(path) === true) {
     throw new Error(`Folder path does not exist`);
   }
 
-  if (typeof startIndex !== "number" || typeof endIndex !== "number") {
-    throw new Error(`Start and End index is required`);
-  }
-
-  async function readFolder(path) {
-    try {
-      const entries = await promises.readdir(path, { withFileTypes: true });
-      return entries;
-    } catch (error) {
-      if (error.code === "EPERM") {
-        logError(`Permission denied for ${path}. Skipping this folder.`);
-        return [];
-      }
-
-      throw error;
-    }
-  }
-
-  // Entries is a list of folder and file data
-  if (pathBeingProcessed !== folderPath) {
-    index = -1;
-    pathBeingProcessed = folderPath
-    pathEntries = await readFolder(folderPath);
-  }
-
-
-  // Making sure the start and end indexes doesn't go over limit
-  startIndex = startIndex < 0 ? 0 : startIndex;
-  endIndex = endIndex > pathEntries.length ? pathEntries.length : endIndex;
-
-  slicedEntries = pathEntries.slice(startIndex, endIndex);
-
-  for (const folderEntry of slicedEntries) {
-    if (streamId !== activeStreamId) {
-      slicedEntries = [];
-      processedEntries = [];
-      return;
-    }
-
-    const entryFullPath = join(folderPath, folderEntry.name);
-
-    if (folderEntry.isFile() && isMediaFile(folderEntry.name) === true) {
-      const isVideo = isVideoFile(entryFullPath);
-
-      // Push the entry to the array instead of sending directly
-      processedEntries.push({
-          index: index++,
-          title: folderEntry.name,
-          destination: folderPath,
-          isMedia: true,
-          path: entryFullPath,
-          thumbnailType: isVideo ? FileGroup.VIDEO : FileGroup.IMAGE,
-          thumbnailPath: entryFullPath
-        });
-
-      continue;
-    }
-
-    if (!folderEntry.isDirectory() === true) {
-      continue;
-    }
-
-    const queue = [entryFullPath]; // Start with the current folder
-    let firstValidParent = null;
-    let foldersTraversed = 0;
-
-    // Perform a breadth-first search (BFS) with a limit of 10 traverses
-    while (queue.length > 0 && foldersTraversed < 10) {
-      if (streamId !== activeStreamId) {
-        slicedEntries = [];
-        processedEntries = [];
-        return;
-      }
-
-      const currentFolderPath = queue.shift();
-      const subFolderEntries = await readFolder(currentFolderPath);
-
-      let containsMedia = false;
-      let containsSubfolders = false;
-
-      for (const subEntry of subFolderEntries) {
-        if (streamId !== activeStreamId) {
-          slicedEntries = [];
-          processedEntries = [];
-          return;
-        }
-
-        if (subEntry.isFile() && isMediaFile(subEntry.name) === true) {
-          containsMedia = true;
-        }
-
-        else if (subEntry.isDirectory() === true) {
-          containsSubfolders = true;
-        }
-
-        // Stop checking further if both media and subfolders are found
-        if (containsMedia && containsSubfolders) break;
-      }
-
-      if (containsMedia) {
-        // Save the first valid parent folder if traversed > 0
-        if (foldersTraversed > 0 && !firstValidParent) {
-          firstValidParent = path.dirname(currentFolderPath);
-        }
-
-        const thumbnailPath = join(
-          currentFolderPath,
-          subFolderEntries.find(subEntry => subEntry.isFile() && isMediaFile(subEntry.name) === true).name
-        );
-
-        // Push the entry to the array instead of sending directly
-        processedEntries.push({
-          index: index++,
-          isMedia: false,
-          destination: folderPath,
-          title: folderEntry.name,
-          path: firstValidParent || currentFolderPath,
-          thumbnailType: isVideoFile(thumbnailPath) ? FileGroup.VIDEO : FileGroup.IMAGE,
-          thumbnailPath: thumbnailPath,
-        });
-
-        break;
-      }
-
-      // If no media found, add subfolders to the queue for further traversal
-      for (const subEntry of subFolderEntries) {
-        if (subEntry.isDirectory() === true) {
-          queue.push(join(currentFolderPath, subEntry.name));
-        }
-      }
-
-      foldersTraversed++;
-    }
-  }
-
-  return processedEntries;
+  await imageryCache.prepareEntries(path);
 });
 
+const { ImageryEntry } = require('./type_definitions.js');
+
 /**
- * Returns a list of media file names in a given directory path.
- * @param {string} folderPath - The path to the folder to scan for media files.
- * @returns {Promise<string[]>} - List of media file names in the folder.
+ * Retrieves the next content from the imagery cache.
+ * @returns {Promise<ImageryEntry|null>} - The next content in the imagery cache.
  */
-ipcMain.handle('directory-media', async (_, path) => {
+ipcMain.handle('next-content', async _ =>
+  imageryCache.next()
+);
+
+/**
+ * Returns a list of media file names (images or videos) in a given directory path.
+ * @param {string} folderPath - The path to the folder to scan for media files.
+ * @returns {Promise<string[]>} - A promise that resolves to an array of media file names in the folder.
+ * @throws {Error} - Throws an error if the folder path does not exist.
+ */
+ipcMain.handle('directory-media', async (_, folderPath) => {
   if (!existsSync(folderPath) === true) {
     throw new Error(`Folder path does not exist: ${folderPath}`);
   }
--- src/scripts/utilities/views/helpers.js
+++ src/scripts/utilities/views/helpers.js
@@ -2,19 +2,24 @@ const { createLogger, transports: _transports } = require('winston');
 const { nativeImage, app, ipcMain, BrowserWindow } = require('electron');
 const { extname, basename, join } = require('path');
 const path = require('path');
+const { promises } = require('fs');
 
-// Enum for object types
+/**
+ * Enum for different file group types.
+ * @readonly
+ * @enum {string}
+ */
 const FileGroup = {
-  DIRECTORY: 'directory',
-  MEDIA: 'media',
-  VIDEO: 'video',
-  IMAGE: 'image'
+  DIRECTORY: 'directory', // Represents a directory.
+  MEDIA: 'media', // Represents a media file.
+  VIDEO: 'video', // Represents a video file.
+  IMAGE: 'image' // Represents an image file.
 };
 
 /**
  * Checks if a file is an image based on its extension.
  * @param {string} fileName - Name of the file to check.
- * @returns {boolean}
+ * @returns {boolean} - Returns true if the file is an image, otherwise false.
  */
 function isImageFile(fileName) {
   const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp'];
@@ -24,7 +29,7 @@ function isImageFile(fileName) {
 /**
  * Checks if a file is a video based on its extension.
  * @param {string} fileName - Name of the file to check.
- * @returns {boolean}
+ * @returns {boolean} - Returns true if the file is a video, otherwise false.
  */
 function isVideoFile(fileName) {
   const videoExtensions = ['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.webm', '.ts'];
@@ -32,9 +37,9 @@ function isVideoFile(fileName) {
 }
 
 /**
- * Checks if a file is a video or image based on its extension.
+ * Checks if a file is a media (image or video) based on its extension.
  * @param {string} fileName - Name of the file to check.
- * @returns {boolean}
+ * @returns {boolean} - Returns true if the file is an image or video, otherwise false.
  */
 function isMediaFile(fileName) {
   return isImageFile(fileName) || isVideoFile(fileName);
@@ -44,11 +49,21 @@ const defaultImagePath = path.resolve(app.getAppPath(), "src/assets/images/load.
 
 /**
  * Creates a thumbnail from a given media path.
- * @param {string} filePath - Path of the file to check.
+ * @param {string} filePath - Path of the file to create the thumbnail from.
  * @param {number} size - Size of the thumbnail.
- * @returns {Promise<String>} The base64 image thumbnail
+ * @returns {Promise<string>} - The base64 image thumbnail.
  */
-ipcMain.handle('get-thumbnail', async (_, filePath, size) => {
+ipcMain.handle('get-thumbnail', async (_, filePath, size) =>
+  await getThumbnail(filePath, size)
+);
+
+/**
+ * Gets a thumbnail for a given media file.
+ * @param {string} filePath - Path of the media file.
+ * @param {number} size - Size of the thumbnail.
+ * @returns {Promise<string>} - The base64 image thumbnail or the default thumbnail path if an error occurs.
+ */
+async function getThumbnail(filePath, size) {
   if (!filePath) {
     logError("Cannot pass an undefined file path to getThumbnail");
     return defaultImagePath;
@@ -65,7 +80,29 @@ ipcMain.handle('get-thumbnail', async (_, filePath, size) => {
     logError(`Error in getting file thumbnail for ${basename(filePath)}, getting default thumbnail instead: ${error}`);
     return defaultImagePath;
   }
-});
+}
+
+/**
+ * Reads the contents of a folder and returns the directory entries.
+ * @param {string} path - Path of the folder to read.
+ * @returns {Promise<fs.Dirent[]>} - A promise that resolves to an array of directory entries.
+ * @throws {Error} - Throws error if folder reading fails due to permission issues or other errors.
+ */
+async function readFolder(path) {
+  try {
+    const entries = await promises.readdir(path, { withFileTypes: true });
+
+    // return entries if no error
+    return entries;
+  } catch (error) {
+    if (error.code === "EPERM") {
+      logError(`Permission denied for ${path}. Skipping this folder.`);
+      return [];
+    }
+
+    throw error;
+  }
+}
 
 const logger = createLogger({
   transports: [
@@ -74,38 +111,69 @@ const logger = createLogger({
 });
 
 /**
- * Logs and saves error to file
- * @param {Error | string} error - an instance of Error or a string
+ * Logs and saves an error to a file.
+ * @param {Error | string} error - An instance of Error or a string describing the error.
  */
 function logError(error) {
-  // console.log(error);
   logger.error(`${new Date().toISOString()} : ${error}`);
 }
 
+/**
+ * Handles the 'print' IPC message and logs the provided message to the console.
+ * @param {Electron.IpcMainInvokeEvent} _ - The event object.
+ * @param {string[]} message - The message to log to the console.
+ */
 ipcMain.handle('print', (_, message) => {
   console.log(message.join(' '));
 });
 
+/**
+ * Joins the provided paths into a single path.
+ * @param {string[]} paths - The array of paths to join.
+ * @returns {string} - The joined path.
+ */
 ipcMain.handle('join-paths', (_, paths) => {
   return join(...paths);
 });
 
+/**
+ * Returns the base name of the provided path.
+ * @param {Electron.IpcMainInvokeEvent} event - The event object.
+ * @param {string} pathArg - The path to extract the base name from.
+ * @returns {string} - The base name of the file or folder.
+ */
 ipcMain.handle('path-basename', (event, pathArg) => {
   return basename(pathArg);
 });
 
+/**
+ * Sets the current window to fullscreen mode.
+ */
 ipcMain.handle('on-fullscreen', () => {
   BrowserWindow.getFocusedWindow().setFullScreen(true);
-})
+});
 
+/**
+ * Sets the current window to non-fullscreen mode.
+ */
 ipcMain.handle('off-fullscreen', () => {
   BrowserWindow.getFocusedWindow().setFullScreen(false);
-})
+});
+
+const defaultThumbnailPath = path.resolve(
+  app.getAppPath(), "src/assets/images/load.webp"
+).replace(/\\/g, '/');
+
+// IPC handler to return the default thumbnail image path.
+ipcMain.handle('default-image', _ => defaultThumbnailPath);
 
 module.exports = {
   FileGroup,
+  defaultThumbnailPath,
+  getThumbnail,
   isImageFile,
   logError,
   isVideoFile,
   isMediaFile,
+  readFolder
 };
