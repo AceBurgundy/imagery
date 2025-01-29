f65f9c819b397862ebe27eba3fa8f6df19a10853
AceBurgundy <Samadriansabalo99@gmail.com>
2025-01-29 22:12:25 +0800

    Refactored for readability.

--- src/scripts/utilities/views/helpers.js
+++ src/scripts/utilities/views/helpers.js
@@ -1,8 +1,8 @@
 const { createLogger, transports: _transports } = require('winston');
-const { nativeImage, app, ipcMain, BrowserWindow } = require('electron');
-const { extname, basename, join } = require('path');
-const path = require('path');
+const { nativeImage, app } = require('electron');
+const { extname, basename } = require('path');
 const { promises, Dirent } = require('fs');
+const path = require('path');
 
 /**
  * Enum for different file group types.
@@ -10,52 +10,50 @@ const { promises, Dirent } = require('fs');
  * @enum {string}
  */
 const FileGroup = {
-  DIRECTORY: 'directory', // Represents a directory.
-  MEDIA: 'media', // Represents a media file.
-  VIDEO: 'video', // Represents a video file.
-  IMAGE: 'image' // Represents an image file.
+  DIRECTORY: 'directory',
+  MEDIA: 'media',
+  VIDEO: 'video',
+  IMAGE: 'image'
 };
 
 /**
  * Checks if a file is an image based on its extension.
- * @param {string} fileName - Name of the file to check.
+ * @param {string} filename - Name of the file to check.
  * @returns {boolean} - Returns true if the file is an image, otherwise false.
  */
-function isImageFile(fileName) {
-  const imageExtensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.webp'];
-  return imageExtensions.includes(extname(fileName).toLowerCase());
-}
+const isImageFile = filename => [
+    '.jpg', '.jpeg', '.png',
+    '.gif', '.webp', '.bmp'
+  ].includes(
+    extname(filename).toLowerCase()
+  );
 
 /**
  * Checks if a file is a video based on its extension.
- * @param {string} fileName - Name of the file to check.
+ * @param {string} filename - Name of the file to check.
  * @returns {boolean} - Returns true if the file is a video, otherwise false.
  */
-function isVideoFile(fileName) {
-  const videoExtensions = ['.mp4', '.mkv', '.avi', '.mov', '.flv', '.wmv', '.webm', '.ts'];
-  return videoExtensions.includes(extname(fileName).toLowerCase());
-}
+const isVideoFile = filename => [
+    '.mp4', '.mkv', '.avi', '.mov',
+    '.flv', '.wmv', '.webm', '.ts'
+  ].includes(
+    extname(filename).toLowerCase()
+  );
 
 /**
  * Checks if a file is a media (image or video) based on its extension.
- * @param {string} fileName - Name of the file to check.
+ * @param {string} filename - Name of the file to check.
  * @returns {boolean} - Returns true if the file is an image or video, otherwise false.
  */
-function isMediaFile(fileName) {
-  return isImageFile(fileName) || isVideoFile(fileName);
-}
-
-const defaultImagePath = path.resolve(app.getAppPath(), "src/assets/images/load.webp").replace(/\\/g, '/');
+const isMediaFile = filename => isImageFile(filename) || isVideoFile(filename);
 
 /**
- * Creates a thumbnail from a given media path.
- * @param {string} filePath - Path of the file to create the thumbnail from.
- * @param {number} size - Size of the thumbnail.
- * @returns {Promise<string>} - The base64 image thumbnail.
+ * Returns the path to the defualt template image
  */
-ipcMain.handle('get-thumbnail', async (_, filePath, size) =>
-  await getThumbnail(filePath, size)
-);
+const defaultThumbnailPath = path
+  .resolve(
+    app.getAppPath(), "src/assets/images/load.webp"
+  ).replace(/\\/g, '/');
 
 /**
  * Gets a thumbnail for a given media file.
@@ -66,19 +64,19 @@ ipcMain.handle('get-thumbnail', async (_, filePath, size) =>
 async function getThumbnail(filePath, size) {
   if (!filePath) {
     logError("Cannot pass an undefined file path to getThumbnail");
-    return defaultImagePath;
+    return defaultThumbnailPath;
   }
 
   try {
-    const media = await nativeImage.createThumbnailFromPath(
+    const thumbnail = await nativeImage.createThumbnailFromPath(
       filePath,
-      { width: size ?? 512, height: size ?? 512 }
+      { width: size ?? 300, height: size ?? 150 }
     );
 
-    return media.toDataURL();
+    return thumbnail.toDataURL();
   } catch (error) {
-    logError(`Error in getting file thumbnail for ${basename(filePath)}, getting default thumbnail instead: ${error}`);
-    return defaultImagePath;
+    logError(`Error in getting file thumbnail for ${basename(filePath)},\ngetting default thumbnail instead.\n${error}`);
+    return defaultThumbnailPath;
   }
 }
 
@@ -90,7 +88,8 @@ async function getThumbnail(filePath, size) {
  */
 async function readFolder(path) {
   try {
-    return await promises.readdir(path, { withFileTypes: true });
+    const entries = await promises.readdir(path, { withFileTypes: true });
+    return entries;
   } catch (error) {
     if (error.code === "EPERM") {
       logError(`Permission denied for ${path}. Skipping this folder.`);
@@ -130,57 +129,8 @@ function logError(error) {
     cause: error.cause
   });
 
-  console.log(error);
+  console.log(`\n${error}`);
 }
-
-/**
- * Handles the 'print' IPC message and logs the provided message to the console.
- * @param {Electron.IpcMainInvokeEvent} _ - The event object.
- * @param {string[]} message - The message to log to the console.
- */
-ipcMain.handle('print', (_, message) => {
-  console.log(message.join(' '));
-});
-
-/**
- * Joins the provided paths into a single path.
- * @param {string[]} paths - The array of paths to join.
- * @returns {string} - The joined path.
- */
-ipcMain.handle('join-paths', (_, paths) => {
-  return join(...paths);
-});
-
-/**
- * Returns the base name of the provided path.
- * @param {Electron.IpcMainInvokeEvent} event - The event object.
- * @param {string} pathArg - The path to extract the base name from.
- * @returns {string} - The base name of the file or folder.
- */
-ipcMain.handle('path-basename', (event, pathArg) => {
-  return basename(pathArg);
-});
-
-/**
- * Sets the current window to fullscreen mode.
- */
-ipcMain.handle('on-fullscreen', () => {
-  BrowserWindow.getFocusedWindow().setFullScreen(true);
-});
-
-/**
- * Sets the current window to non-fullscreen mode.
- */
-ipcMain.handle('off-fullscreen', () => {
-  BrowserWindow.getFocusedWindow().setFullScreen(false);
-});
-
-const defaultThumbnailPath = path.resolve(
-  app.getAppPath(), "src/assets/images/load.webp"
-).replace(/\\/g, '/');
-
-// IPC handler to return the default thumbnail image path.
-ipcMain.handle('default-image', _ => defaultThumbnailPath);
 
 module.exports = {
   FileGroup,
